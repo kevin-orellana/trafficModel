14d6d8686b685f8b20b4513baa1da080e23e328e
Chrisaric <chrisaric@github.com>
2018-04-04 20:48:33 -0400

    s

--- insertnewcar.m
+++ insertnewcar.m
@@ -1,24 +1,33 @@
-% insertnewcar.m
-c = firstcar(b);
-if (c == 0 || p(nc) > p(c))
-    nextcar(nc) = c;
-    firstcar(b) = nc;
-    if ((c == 0)
-        lastcar(b) = nc;
-    end
-elseif p(nc) <= p(lastcar(b))
-    nextcar(lastcar(b)) = nc;
-    lastcar(b) = nc;
-else
-    ca = c
-    c = nextcar(c)
-    while (p(nc) <= p(c))
-        ca = c;
-        c = nextcar(c)
-    end
-    nextcar(ca) = nc;
-    nextcar(nc) = c;
-end
+function  [firstcar, lastcar] = insertnewcar(firstcar, lastcar, num_car, p, b)
 
+    car = firstcar(b);
+    % p: distance from start intersection to car
+    if (car == 0 || p(num_car) > p(car)):
+        nextcar(num_car) = c;
+        firstcar(b) = num_cars;
+        if (car == 0):
+            lastcar(b) = num_cars;
+        end
+        
+    else if p(num_car) <= p(lastcar(b)):
+        nextcar(lastcar(b)) = num_car;
+        lastcar(b) = num_car;
+    
+    else:
+        ca = car;
+        car = nextcar(car);
+        
+        while (p(nc) <= p(c))
+            ca = c;
+            c = nextcar(c);
+            
+        end
+        
+        nextcar(ca) = nc;
+        nextcar(nc) = c;
+        
+     end
+     
+ end
         
         
